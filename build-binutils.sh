983a10164878058d1b16afcbf5e796d1d0ead836
Max Filippov <jcmvbkbc@gmail.com>
2011-09-05 00:06:32 +0400

    Add --disable-werror to build-binutils
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- build-binutils.sh
+++ build-binutils.sh
@@ -7,7 +7,7 @@ export LANG=C
 
 mkdir $BINUTILS
 cd $BINUTILS
-../../$BINUTILS/configure --prefix=$PREFIX --target=$TARGET --enable-shared
+../../$BINUTILS/configure --prefix=$PREFIX --target=$TARGET --enable-shared --disable-werror
 make $PARALLEL
 #make check
 make install
