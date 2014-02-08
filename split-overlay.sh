863760a406c56f04f123cc8832444e31e3363fc9
Max Filippov <jcmvbkbc@gmail.com>
2014-02-08 13:14:45 +0400

    split-overlay: add script to repack overlays
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ split-overlay.sh
@@ -0,0 +1,69 @@
+#! /bin/bash -ex
+
+CORE="$1"
+SRC="$2"
+DST="$3"
+
+DIR=`mktemp -d`
+trap 'rm -rf "$DIR"' EXIT
+tar -xf "$SRC" -C "$DIR"
+
+split_binutils()
+{
+	A="$DIR"/binutils
+	B="$DIR"/res
+
+	mkdir -p "$B"/{bfd,include}
+	cp "$A"/xtensa-modules.c "$B"/bfd
+	cp "$A"/xtensa-config.h "$B"/include
+
+	tar -czf "$DST"/binutils-xtensa_${CORE}.tgz -C "$B" .
+	rm -rf "$B"
+}
+
+split_gcc()
+{
+	A="$DIR"/gcc
+	B="$DIR"/res
+
+	mkdir -p "$B"/include
+	cp "$A"/xtensa-config.h "$B"/include
+
+	tar -czf "$DST"/gcc-xtensa_${CORE}.tgz -C "$B" .
+	rm -rf "$B"
+}
+
+split_gdb()
+{
+	A="$DIR"/gdb
+	B="$DIR"/res
+
+	mkdir -p "$B"/{bfd,gdb/{gdbserver,regformats},include}
+	cp "$A"/xtensa-modules.c "$B"/bfd
+	cp "$A"/xtensa-config.h "$B"/include
+	cp "$A"/xtensa-config.c "$B"/gdb
+	cp "$A"/xtensa-xtregs.c "$B"/gdb
+	cp "$A"/xtensa-xtregs.c "$B"/gdb/gdbserver
+	cp "$A"/xtensa-regmap.c "$B"/gdb/gdbserver
+	cp "$A"/reg-xtensa.dat "$B"/gdb/regformats
+
+	tar -czf "$DST"/gdb-xtensa_${CORE}.tgz -C "$B" .
+	rm -rf "$B"
+}
+
+split_linux()
+{
+	A="$DIR"/config
+	B="$DIR"/res
+
+	mkdir -p "$B"/$CORE/include/variant
+	cp "$A"/{core.h,tie.h,tie-asm.h} "$B"/$CORE/include/variant
+
+	tar -czf "$DST"/linux-xtensa_${CORE}.tgz -C "$B" .
+	rm -rf "$B"
+}
+
+split_binutils "$@"
+split_gcc "$@"
+split_gdb "$@"
+split_linux "$@"
