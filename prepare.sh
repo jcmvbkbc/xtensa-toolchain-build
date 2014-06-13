c50dbd0252e44944e47fc173a939949ec792e096
Max Filippov <jcmvbkbc@gmail.com>
2014-06-13 07:53:59 +0400

    prepare: overlays: switch to consolidated overlays
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -11,6 +11,6 @@
 #( cd $NEWLIB ; patch -p1 < ../$NEWLIB.patch )
 #ln -s ../$NEWLIB/{newlib,libgloss} $GCC
 
-tar -xzf overlays/binutils-xtensa_$1.tgz -C $BINUTILS
-tar -xzf overlays/gcc-xtensa_$1.tgz -C $GCC
-tar -xzf overlays/gdb-xtensa_$1.tgz -C $GDB
+tar -xf overlays/xtensa_$1.* -C $BINUTILS --strip-components=1 binutils
+tar -xf overlays/xtensa_$1.* -C $GCC --strip-components=1 gcc
+tar -xf overlays/xtensa_$1.* -C $GDB --strip-components=1 gdb
